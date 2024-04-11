74d433c8e70db912f59b73b7c78c210688c078bb
Ricardo Guerra <RikkuG491@gmail.com>
2024-04-11 15:08:33 -0400

    Set up project backend and frontend

--- server/app.py
+++ server/app.py
@@ -3,20 +3,26 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, make_response
 from flask_restful import Resource
 
 # Local imports
 from config import app, db, api
 # Add your model imports
-
+from models import Flight
 
 # Views go here!
+class AllFlights(Resource):
+
+    def get(self):
+        response_body = [flight.to_dict(only=('id', 'airline', 'image')) for flight in Flight.query.all()]
+        return make_response(response_body, 200)
+
+api.add_resource(AllFlights, '/flights')
 
 @app.route('/')
 def index():
     return '<h1>Project Server</h1>'
-
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -4,3 +4,9 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+class Flight(db.Model, SerializerMixin):
+    __tablename__ = 'flights'
+
+    id = db.Column(db.Integer, primary_key=True)
+    airline = db.Column(db.String, nullable=False)
+    image = db.Column(db.String, nullable=False)
